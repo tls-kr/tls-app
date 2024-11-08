7d440c7100c7166b78335a48ac71c7bd5ee2a277
ChristianW <cwittern@yahoo.com>
2024-11-08 13:29:33 +0900

    Citations first version ready.

--- resources/scripts/tls-citations.js
+++ resources/scripts/tls-citations.js
@@ -5,28 +5,32 @@
 
 // the dev server needs a different prefix
 
-const urlprefix =  window.location.host.includes('8443') ? '/exist/apps/krx-app/' : 'krx/'
+const urlprefix =  window.location.host.includes('8443') ? '/exist/apps/tls-app/' : '/'
 
 
 
 
 function do_citation() {
-  var location = window.location.search;
+  var item = $('#input-target').val();
+  var perspective = $('#select-perspective').val()
+  var group1 = $('#select-grouping-1').val()
+  var group2 = $('#select-grouping-2').val()
   $.ajax({
   type : "GET",
-  dataType : "json",
-  url : urlprefix +  "itemcount"+location, 
+  dataType : "html",
+  url : urlprefix +  "citations/"+perspective+"/"+group1+"?item="+item+"&group2="+group2, 
   success : function(resp){
-    $('#krx_search').html(resp.responseText);
+    $('#cit-results').html(resp.responseText);
     },
   error: function(resp){
     console.log('an error has occurred?!', resp.status, urlprefix)
     },
   complete : function(resp){
   //  var res = String(resp);
-    $('#krx_search').html(resp.responseText);
+    $('#cit-results').html(resp.responseText);
   }
   });
+  $('#cit-results').html('Processing data, please wait for a moment.')
 };
 
 
